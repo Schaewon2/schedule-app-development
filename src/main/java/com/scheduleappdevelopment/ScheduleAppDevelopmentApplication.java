6f1f7f79fb5d6445798b7acb9eb7ffdf21bbd3d7
Schaewon2 <schaewon1206@gmail.com>
2026-04-17 16:00:35 +0900

    Feat: 스프링부트 프로젝트 초기 설정 및 프로젝트 구조 생성

---
+++ src/main/java/com/scheduleappdevelopment/ScheduleAppDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.scheduleappdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleAppDevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleAppDevelopmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scheduleappdevelopment/ScheduleAppDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheduleappdevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleAppDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
